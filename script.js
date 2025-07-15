9ae3fce003c55a36e980da87d53815032e333fcb
Soufiane Elmiloudi <elmiloudisofyan@gmail.com>
2025-07-16 00:44:34 +0100

    add new design

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 window.onload = function () {
             setTimeout(function () {
                 document.getElementById("loading").style.display = "none";
-                document.querySelector(".loader").style.display = "block";
             }, 3000);
             };
