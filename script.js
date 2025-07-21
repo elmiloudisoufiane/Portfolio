3281700068345b6e59829a3ddd7fdb7424a0fe2f
Soufiane Elmiloudi <elmiloudisofyan@gmail.com>
2025-07-21 01:38:41 +0100

    change desgin

--- script.js
+++ script.js
@@ -1,5 +1,48 @@
+const loading = document.createElement('div');
+const spinner = document.createElement('div');
+const title = document.createElement('h1');
+
+document.body.appendChild(loading);
+
+        loading.appendChild(spinner);
+        loading.appendChild(title);
+
+        loading.id = "loading";
+        loading.style.width = "100vw";
+        loading.style.height = "100vh";
+        loading.style.display = "flex";
+        loading.style.justifyContent = "center";
+        loading.style.alignItems = "center";
+        loading.style.position = "fixed";
+        loading.style.top = "0";
+        loading.style.left = "0";
+        loading.style.zIndex = "1000";
+        loading.style.fontFamily = "Arial, sans-serif";
+        loading.style.fontSize = "1em";
+        loading.style.color = "#fff";
+        loading.style.textAlign = "center";
+        loading.style.backgroundColor = "black";
+        loading.id = "loading";
+        loading.style.flexDirection="column"
+        
+        title.id = "title";
+        title.innerText = "Loading";
+        title.id = "title";
+        title.innerText = "Loading Portfolio";
+        title.style.color = "#fff";
+
+        spinner.className = "spinner";
+        spinner.style.width = "30px";
+        spinner.style.height = "30px";
+        spinner.style.border = "5px solid #f3f3f3";
+        spinner.style.borderTop = "5px solid #6a01d4";
+        spinner.style.borderRadius = "50%";
+        spinner.style.animation = "spin 2s linear infinite";
+
+
+
 window.onload = function () {
             setTimeout(function () {
-                document.getElementById("loading").style.display = "none";
+                loading.style.display = "none";
             }, 3000);
             };
