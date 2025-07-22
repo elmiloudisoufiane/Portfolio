b8992e43fceeae9c00fdc8ada43c7e25635b84e4
Soufiane Elmiloudi <elmiloudisofyan@gmail.com>
2025-07-22 02:19:01 +0100

    add dark mode and responsive design

--- script.js
+++ script.js
@@ -41,6 +41,24 @@ document.body.appendChild(loading);
 
 
 
+const toggleBtn = document.getElementById("toggle");
+const lightIcon = document.querySelector(".fa-sun");
+const darkIcon = document.querySelector(".fa-moon");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("light-mode");
+
+
+
+  if (document.body.classList.contains("light-mode")) {
+    lightIcon.style.display = "none";
+    darkIcon.style.display = "block";
+  } else {
+    lightIcon.style.display = "block";
+    darkIcon.style.display = "none";
+  }
+});
+
 window.onload = function () {
             setTimeout(function () {
                 loading.style.display = "none";
